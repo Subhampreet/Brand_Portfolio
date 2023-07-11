9bc974f749823b462a55b08a27b285a73027b333
Subhampreet <subhampreet18@gmail.com>
2023-07-11 21:32:10 +0530

    Test Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
           <MenuButton>Menu</MenuButton>
           <MenuButton>Contact</MenuButton>
           <MenuButton>PROJECTS</MenuButton>
+          <MenuButton>About Me</MenuButton>
       </main>
     </div>
   );
