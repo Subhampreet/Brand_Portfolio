f1808cdebde04ec34e730144890c08d15e603efc
Subhampreet <subhampreet18@gmail.com>
2023-07-27 19:54:46 +0530

    Build 0.1.3 : Marquee Tag 🎉

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '@/components/Header/Header'
+import Marquee from 'react-fast-marquee'
 
 function About() {
   return (
@@ -20,6 +21,11 @@ function About() {
           <div className='left'>
             <h5>I make websites and photos, and I do it well.</h5>
             <p>With a passion for music, the stage and graphic design, I enjoy working with people who value aesthetics, who understand the power of simplicity and who know that a good customer experience is paramount.<br /><br/>By keeping your identity at the heart of my approach, my goal is to find the simplest solution to meet your needs. <br /><br/>My clients call on me for my efficiency, versatility and ease of communication (and also because I make incredible cookies, the secret is to put a lot of semi-salted butter in them).</p>
+            <Marquee className='techstack-tags' pauseOnHover>
+              <div className='tag'>Next.JS</div>
+              <div className='tag yellow'>JavaScript</div>
+              <div className='tag green'>DotNet</div>
+            </Marquee>
           </div>
           <div className='right'>
             <img src="./about_02.jpg" />
