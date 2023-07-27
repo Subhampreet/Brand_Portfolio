089480e67a1158a1fbe3aff96cfbd65caf3dfb41
Subhampreet <subhampreet18@gmail.com>
2023-07-27 19:31:31 +0530

    Build 0.1.2 : About Me Page - Banner, Info Section

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,30 @@ function About() {
   return (
     <div className='AboutMe'>
       <Header />
-      <p>Discover our numerous opportunities</p>
+      <div className='banner'>
+        <div className='body'>
+          <div className='top'>
+            <p>With a passion for music, the stage and graphic design, <br /><span>I enjoy working with people who value aesthetics</span></p>
+          </div>
+          <div className='bottom'>
+            <img src="./aboutbanner.png" />
+          </div>
+        </div>
+      </div>
+      <div className='info'>
+        <div className='body'>
+          <div className='left'>
+            <h5>I make websites and photos, and I do it well.</h5>
+            <p>With a passion for music, the stage and graphic design, I enjoy working with people who value aesthetics, who understand the power of simplicity and who know that a good customer experience is paramount.<br /><br/>By keeping your identity at the heart of my approach, my goal is to find the simplest solution to meet your needs. <br /><br/>My clients call on me for my efficiency, versatility and ease of communication (and also because I make incredible cookies, the secret is to put a lot of semi-salted butter in them).</p>
+          </div>
+          <div className='right'>
+            <img src="./about_02.jpg" />
+            <div className='bottom'>
+              LET’S KNOW EACH OTHER <span>MORE</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
