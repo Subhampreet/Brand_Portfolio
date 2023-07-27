76daaeadafc6b22164e3a78c3f73a012c618c0ef
Subhampreet <subhampreet18@gmail.com>
2023-07-28 03:54:45 +0530

    Build 0.1.4 : Image Hover Effect Added

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,16 +16,22 @@ function About() {
           </div>
         </div>
       </div>
-      <div className='info'>
+      <div className='info_me'>
         <div className='body'>
           <div className='left'>
             <h5>I make websites and photos, and I do it well.</h5>
             <p>With a passion for music, the stage and graphic design, I enjoy working with people who value aesthetics, who understand the power of simplicity and who know that a good customer experience is paramount.<br /><br/>By keeping your identity at the heart of my approach, my goal is to find the simplest solution to meet your needs. <br /><br/>My clients call on me for my efficiency, versatility and ease of communication (and also because I make incredible cookies, the secret is to put a lot of semi-salted butter in them).</p>
-            <Marquee className='techstack-tags' pauseOnHover>
+           
+            {/* <Marquee className='techstack-tags' pauseOnHover>
               <div className='tag'>Next.JS</div>
               <div className='tag yellow'>JavaScript</div>
               <div className='tag green'>DotNet</div>
             </Marquee>
+            <Marquee className='techstack-tags' pauseOnHover direction='right'>
+              <div className='tag'>MONGODB</div>
+              <div className='tag yellow'></div>
+              <div className='tag green'>DotNet</div>
+            </Marquee> */}
           </div>
           <div className='right'>
             <img src="./about_02.jpg" />
